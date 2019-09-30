d779c7cf9aeb368e5febff990a3c8e2f3504d4a2
brookesjw1 <brookesjw1@gmail.com>
2019-09-30 16:49:54 +0100

    get dogs modified to include query by distance

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,34 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const { GeoCollectionReference, GeoFirestore, GeoQuery, GeoQuerySnapshot } = require('geofirestore');
+firebase.initializeApp();
 
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-const db = admin.firestore;
+const firestore = firebase.firestore();
+const geofirestore = new GeoFirestore(firestore);
+const usersCollection = geofirestore.collection("users");
+const dogsCollection = geofirestore.collection("dogs");
 
 exports.addUser = functions.https.onRequest(async (req, res) => {
     const { id, firstName, surname, location, radiusPref, employmentStatus, activityLevel, hasChildren, hasDogs, dob, gender, sizePref, avatar } = req.body;
 
-    const writeResult = await db().collection('users').doc(id).set( { firstName, surname, location: new db.GeoPoint(location[0], location[1]), radiusPref, employmentStatus, activityLevel, hasChildren, hasDogs, dob: db.Timestamp.fromDate(new Date(dob)), gender, sizePref, avatar });
-    res.json({ result: `User ${firstName} ${surname} added`});
+    const addedUser = await usersCollection.doc(id).set({
+        firstName, surname, radiusPref, employmentStatus, activityLevel, hasChildren, hasDogs, dob: firebase.firestore.Timestamp.fromDate(new Date(dob)), gender, sizePref, avatar, coordinates: new firebase.firestore.GeoPoint(location[0], location[1])
+    })
+    res.json({ result: `User ${firstName} ${surname} added`})
 });
 
 exports.addDog = functions.https.onRequest(async (req, res) => {
     const { name, dob, breed, size, goodWithChildren, exerciseLevel, description, goodWithOtherDogs, gender, photos, videos, centre_id, location } = req.body;
 
-    const addedDog = await db().collection('dogs').add({ name, dob: db.Timestamp.fromDate(new Date(dob)), breed, size, goodWithChildren, exerciseLevel, description, goodWithOtherDogs, gender, photos, videos, centre_id, location: new db.GeoPoint(location[0], location[1]) });
+    const addedDog = await dogsCollection.add({ name, dob: firebase.firestore.Timestamp.fromDate(new Date(dob)), breed, size, goodWithChildren, exerciseLevel, description, goodWithOtherDogs, gender, photos, videos, centre_id, coordinates: new firebase.firestore.GeoPoint(location[0], location[1]) });
     res.json({ result: `Dog with ID ${addedDog.id} added`})
 });
 
-exports.getDogs = functions.https.onRequest((req, res) => {
-    return db().collection('dogs').get()
-    .then((snapshot) => {
-        const dogs = snapshot.docs.map(doc => doc.data());
+exports.getDogs = functions.https.onRequest(async (req, res) => {
+    const radius = Number(req.query.radius);
+    const lat = Number(req.query.lat);
+    const lon = Number(req.query.lon);
+    const query = await dogsCollection.near({ center: new firebase.firestore.GeoPoint(lat || 0, lon || 0), radius: radius || 100000000000000 }).get()
+        const dogs = query.docs.map(doc => doc.data());
         return res.json({ dogs })
-    })
 })
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,14 +3,20 @@ const axios = require('axios');
 const { expect } = chai;
 
 describe('getDogs', () => {
+    let unfilteredLength;
     it('returns a list of dogs', () => {
         return axios.get("https://us-central1-rescuemetest-4a629.cloudfunctions.net/getDogs")
         .then((res) => {
+            unfilteredLength = res.data.dogs.length;
             expect(res.status).to.equal(200);
             expect(res.data.dogs).to.be.an('array');
         });
     });
     it('filters out any dogs outside the specified radius', () => {
-        
+        return axios.get("https://us-central1-rescuemetest-4a629.cloudfunctions.net/getDogs?radius=50&lat=53.4808&lon=-2.2426")
+        .then((res) => {
+            expect(res.status).to.equal(200);
+            expect(res.data.dogs.length).to.be.lessThan(unfilteredLength);
+        });
     });
 });
